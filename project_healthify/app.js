424e120aac8b08203e1e08a65ee9afe5e3cba0cb
Saptarshi Chakraborty <saptarshi1999chakraborty@gmail.com>
2021-06-15 10:53:01 +0530

    Minor

--- project_healthify/app.js
+++ project_healthify/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
